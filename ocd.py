06d1167b8d1e65aa92de6a8f82bcd83752e2dfc7
Hekal <101211945+hekalet@users.noreply.github.com>
2023-03-04 19:46:31 -0800

    fun

---
+++ ocd.py
@@ -0,0 +1,7 @@
+answer = input("Hello, I'm OCDeezNutsGPT. Say 'OCD'.     ")
+if answer == "OCD":
+    print("OC DEEZ NUTS!!!!!! Restart the program.")
+
+elif answer != "OCD":
+    print("Say OCD. Restart the program.")
+
