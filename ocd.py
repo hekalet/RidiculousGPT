522974cf3c7247da664b0eaa5b0fb2726a405750
Hekal <101211945+hekalet@users.noreply.github.com>
2023-03-06 13:04:59 -0800

    Delete ocd.py

--- ocd.py
+++
@@ -1,7 +0,0 @@
-answer = input("helo i'm ridiculous gpt")
-if answer == "OCD":
-    print("OC DEEZ NUTS!!!!!!")
-
-elif answer != "OCD":
-    print("The central nervous system (CNS) is the part of the nervous system consisting primarily of the brain and spinal cord. The CNS is so named because the brain integrates the received information and coordinates and influences the activity of all parts of the bodies of bilaterally symmetric and triploblastic animals—that is, all multicellular animals except sponges and diploblasts. It is a structure composed of nervous tissue positioned along the rostral (nose end) to caudal (tail end) axis of the body and may have an enlarged section at the rostral end which is a brain. Only arthropods, cephalopods and vertebrates have a true brain (precursor structures exist in onychophorans, gastropods and lancelets).")
-
