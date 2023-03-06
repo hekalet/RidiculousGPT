743fbafb071d86fbd6528b2715442f195b93079f
Hekal <101211945+hekalet@users.noreply.github.com>
2023-03-06 13:04:46 -0800

    Move to ports branch

--- ports/webport(html,css,js)/script.js
+++
@@ -1,8 +0,0 @@
-let foo = prompt('helo im ridiculous gpt');
-
-if (foo == "OCD") {
-    alert("OC DEEZ NUTS!!!!!!")
-} else { 
-    alert("The central nervous system (CNS) is the part of the nervous system consisting primarily of the brain and spinal cord. The CNS is so named because the brain integrates the received information and coordinates and influences the activity of all parts of the bodies of bilaterally symmetric and triploblastic animals—that is, all multicellular animals except sponges and diploblasts. It is a structure composed of nervous tissue positioned along the rostral (nose end) to caudal (tail end) axis of the body and may have an enlarged section at the rostral end which is a brain. Only arthropods, cephalopods and vertebrates have a true brain (precursor structures exist in onychophorans, gastropods and lancelets).")
-}
-
